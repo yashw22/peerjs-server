f1c20e2141a5a9deb9fc4a804771c1ebd6083067
Yash Wardhan <45715307+yashw22@users.noreply.github.com>
2024-11-23 02:02:37 -0500

    removed express

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
-const express = require("express");
-const { ExpressPeerServer } = require("peer");
+const { PeerServer } = require('peer');
 
-const app = express();
-
-app.get("/", (req, res, next) => res.send("Hello world!"));
-
-// =======
-
-const server = app.listen(9000);
-
-const peerServer = ExpressPeerServer(server, {
-    path: "/deckdetective",
+const peerServer = PeerServer({
+    port: process.env.PORT || 9000,
+    path: '/deckdetective',
 });
 
-app.use("/peerjs", peerServer);
 console.log('PeerJS server is running...');
